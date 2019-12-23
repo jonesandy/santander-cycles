8a7cacce7a95a2e21a106eae320ee648753e0014
Andy Jones <andytjones@icloud.com>
2019-12-23 22:31:56 +0000

    Feature - Error raised if no bikes available to release.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,19 +12,31 @@ describe DockingStation do
     expect(station).to respond_to(:dock).with(1).argument
   end
 
+
   it 'responds to #bike' do
     expect(station).to respond_to(:bike)
   end
 
-  it 'releases a working bike' do
-    bike = station.release_bike
-    expect(bike).to be_working
-  end
-  
-  it 'docks a bike' do
-    bike = station.release_bike
-    station.dock(bike)
-    expect(station.bike).to eq(bike)
+  describe '#release_bike' do
+
+    it 'releases a bike' do
+      bike = Bike.new
+      station.dock(bike)
+
+      expect(station.release_bike).to eq(bike)
+    end
+
+    it 'releases a working bike' do
+      bike = Bike.new
+      station.dock(bike)
+
+      expect(bike).to be_working
+    end
+
+    it 'raises an error when no bikes available' do
+      expect { station.release_bike }.to raise_error "No bikes available!"
+    end
+
   end
 
 end
