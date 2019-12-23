83d46039d5d04335f587829d17b171dc2238e43d
Andy Jones <andytjones@icloud.com>
2019-12-23 21:46:05 +0000

    Feature - Station releases working bike.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,10 @@ describe DockingStation do
   it 'responds to #release_bike' do
     expect(station).to respond_to(:release_bike)
   end
+
+  it 'releases a working bike' do
+    bike = station.release_bike
+    expect(bike).to be_working
+  end
+
 end
