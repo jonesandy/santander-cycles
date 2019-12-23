877ab2c93f3114fde6654bb6a1d22a55b6bb23f5
Andy Jones <andytjones@icloud.com>
2019-12-23 23:49:02 +0000

    Feature - Ability to mark bikes as broken.

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -6,4 +6,14 @@ describe Bike do
 
   it { is_expected.to respond_to :working? }
 
+  context 'broken bike' do
+
+    it 'reports broken bike' do
+      bike.report_broken
+
+      expect(bike).not_to be_working
+    end
+
+  end
+
 end
