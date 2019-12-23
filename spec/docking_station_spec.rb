7520d9f34377ecda3dd2f714cf4621a641327c56
Andy Jones <andytjones@icloud.com>
2019-12-23 22:12:40 +0000

    Feat - docks a bike in the station.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,14 +3,28 @@ require 'docking_station'
 describe DockingStation do
 
   let(:station) { DockingStation.new }
-
+    
   it 'responds to #release_bike' do
     expect(station).to respond_to(:release_bike)
+  end
+
+  it 'responds to #dock' do
+    expect(station).to respond_to(:dock).with(1).argument
+  end
+
+  it 'responds to #bike' do
+    expect(station).to respond_to(:bike)
   end
 
   it 'releases a working bike' do
     bike = station.release_bike
     expect(bike).to be_working
+  end
+  
+  it 'docks a bike' do
+    bike = station.release_bike
+    station.dock(bike)
+    expect(station.bike).to eq(bike)
   end
 
 end
